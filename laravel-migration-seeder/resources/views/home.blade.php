80ea74b59fd4616c4c95849decbce1b371c8039e
frame88 <fleuzzi97@gmail.com>
2022-02-23 17:08:13 +0100

    scaffolding + traccia

---
+++ laravel-migration-seeder/resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>traccia</title>
+</head>
+<body>
+{{-- Esercizio di oggi:
+nome repo: laravel-migration-seeder
+Creiamo una tabella trains e relativa Migration
+Ogni treno dovrà avere:
+Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+È probabile che siano necessarie altre colonne per far funzionare la tabella nel modo corretto :occhiolino:
+Inserite inizialmente i dati tramite PhpMyAdmin.
+Create Model relativo ed un Controller per mostrare nella home page tutti i treni che sono in partenza dalla data odierna.
+Lunedì lavoreremo sulla stessa repo per aggiungere i seeder. --}}
+</body>
+</html>
